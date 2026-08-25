16781202888906ea53c9efd47be2e132d1b1b654
Robin Shibambo <RobinNShibambo@gmail.com>
2026-08-25 15:07:50 +0200

    chore: enable dark theme by default (add 'dark' class on html)
    - Force the Tailwind dark color theme site-wide by adding the `dark` class to the root html element.

--- main.jsx
+++ main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// Force dark theme for the entire app by adding the Tailwind "dark" class to <html>
+if (typeof document !== 'undefined') {
+  document.documentElement.classList.add('dark')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
