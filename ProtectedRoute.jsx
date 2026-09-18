ceacb36299c60fd9ea03a045f405541894e12039
Robin Shibambo <RobinNShibambo@gmail.com>
2026-09-18 12:56:12 +0200

    Update import path for useAuth in ProtectedRoute

--- ProtectedRoute.jsx
+++ ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useAuth } from '@/lib/AuthContext';
+import { useAuth } from './AuthContext.jsx';
 import UserNotRegisteredError from './UserNotRegisteredError';
 
 const DefaultFallback = () => (
